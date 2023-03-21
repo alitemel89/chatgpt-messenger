007e550ed47aa81915fd42e5175a61f5cf22e42c
alitemel89 <alitemel89.ce@gmail.com>
2023-03-21 11:45:33 +0300

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-white">
       <h1 className="text-5xl font-bold mb-20">ChatGpt</h1>
-      <div className="mb-8">
+      <div className="mb-8 sm:hidden">
       <NewChat />
       </div>
       <div className="flex space-x-2 text-center mx-4">
